a16e830abc22667a7a1828e012e38f3a986a4895
James Pond <james@cipher.host>
2024-03-12 19:33:04 -0300

    Use rejection method for RandomIndex function
    This change improves the internal RandomIndex function by implementing
    the rejection method to eliminate bias in the generated random indices.
    
    Signed-off-by: James Pond <james@cipher.host>

--- internal/cryptoutil/cryptoutil.go
+++ internal/cryptoutil/cryptoutil.go
@@ -13,27 +13,26 @@ import (
 const ErrDivisionByZero xerrors.Error = "division by zero; input must be greater than zero"
 
 // RandomIndex generates a cryptographically secure random index in the range
-// [0, n-1]. It tries to eliminate bias by discarding values that would cause
-// bias due to the modulo operation.
+// [0, n-1]. It tries to eliminate bias by using the rejection method.
 func RandomIndex(n int, randReader io.Reader) (int, error) {
 	if n <= 0 {
 		return 0, ErrDivisionByZero
 	}
 
 	var (
-		max       = ^uint32(0) // Max value for uint32
-		threshold = max - (max % uint32(n))
+		max       = ^uint64(0)
+		threshold = max - max%uint64(n)
+		b         [8]byte
 	)
 
 	for {
-		var r uint32
-
-		if err := binary.Read(randReader, binary.LittleEndian, &r); err != nil {
+		if _, err := io.ReadFull(randReader, b[:]); err != nil {
 			return 0, fmt.Errorf("%w", err)
 		}
 
-		if r < threshold {
-			return int(r % uint32(n)), nil
+		num := binary.BigEndian.Uint64(b[:])
+		if num < threshold {
+			return int(num % uint64(n)), nil
 		}
 	}
 }
--- internal/cryptoutil/cryptoutil_test.go
+++ internal/cryptoutil/cryptoutil_test.go
@@ -16,21 +16,19 @@ func TestRandomIndex(t *testing.T) {
 		name       string
 		n          int
 		input      []byte
-		expected   int
 		expectErr  bool
 		errMessage error
 	}{
 		{
 			name:      "ValidRandomIndex",
 			n:         10,
-			input:     []byte{0x04, 0x00, 0x00, 0x00},
-			expected:  4,
+			input:     []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04},
 			expectErr: false,
 		},
 		{
 			name:      "ExceedsThreshold",
 			n:         10,
-			input:     append([]byte{0xFF, 0xFF, 0xFF, 0xFF}, 0x04, 0x00, 0x00, 0x00),
+			input:     []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04},
 			expectErr: false,
 		},
 		{
@@ -43,7 +41,7 @@ func TestRandomIndex(t *testing.T) {
 		{
 			name:       "ZeroValueN",
 			n:          0,
-			input:      []byte{0x01, 0x00, 0x00, 0x00},
+			input:      []byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 			expectErr:  true,
 			errMessage: cryptoutil.ErrDivisionByZero,
 		},
@@ -51,32 +49,30 @@ func TestRandomIndex(t *testing.T) {
 
 	for _, tt := range tests {
 		tt := tt
-
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
 			randReader := bytes.NewReader(tt.input)
 			index, err := cryptoutil.RandomIndex(tt.n, randReader)
+
 			if tt.expectErr {
 				if err == nil {
 					t.Fatalf("expected error, got nil")
 				}
+
 				if !errors.Is(err, tt.errMessage) {
 					t.Fatalf("expected error %v, got %v", tt.errMessage, err)
 				}
+
 				return
 			}
+
 			if err != nil {
 				t.Fatalf("unexpected error: %v", err)
 			}
-			if tt.name == "ExceedsThreshold" {
-				if index < 0 || index >= tt.n {
-					t.Fatalf("index out of valid range [0, %d), got %d", tt.n, index)
-				}
-			} else {
-				if index != tt.expected {
-					t.Fatalf("expected %d, got %d", tt.expected, index)
-				}
+
+			if index < 0 || index >= tt.n {
+				t.Fatalf("index out of valid range [0, %d), got %d", tt.n, index)
 			}
 		})
 	}
