64c6bd387cc9093ef83868fbd038709aa83d8647
James Pond <james@cipher.host>
2024-05-15 20:03:39 -0300

    Convert dynamic error into constant
    Signed-off-by: James Pond <james@cipher.host>

--- diceware_internal_test.go
+++ diceware_internal_test.go
@@ -2,14 +2,17 @@ package acopw
 
 import (
 	"crypto/rand"
-	"errors"
 	"testing"
+
+	"git.sr.ht/~jamesponddotco/xstd-go/xerrors"
 )
+
+const ErrGeneric xerrors.Error = "error"
 
 type mockReader struct{}
 
 func (*mockReader) Read(_ []byte) (n int, err error) {
-	return 0, errors.New("mock read error")
+	return 0, ErrGeneric
 }
 
 func TestDiceware_Generate_Panic(t *testing.T) { //nolint:paralleltest // we're modifying rand.Reader
